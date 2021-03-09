6ed3593696e0e270a1d2a4a24e2a6e4b72955d84
Nerea García <nereagarcia.c@gmail.com>
2021-03-09 18:46:18 +0100

    aplicar bootstrap a main y footer de layout

--- src/App.js
+++ src/App.js
@@ -3,47 +3,47 @@ import { Container, Row, Col } from "react-bootstrap";
 function App() {
   return (
     <Container as="body">
-      <Container fluid className="contenedor-general">
-        <Col as="header" className="px-3 py-4">
-          <Col as="h1" className="mb-0">Título</Col>
-        </Col>
-        <main className="d-md-flex flex-grow-1">
-          <div className="principal px-3 py-4 col-md-8">
-            <h2>Titular de la noticia</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio itaque
+      <Container fluid className="contenedor-general p-0 h-100">
+        <Row as="header" className="px-3 py-4">
+          <Row as="h1" className="mb-0 px-3">Título</Row>
+        </Row>
+        <Row>
+          <Col md={8} className="principal px-3 py-4">
+            <Row as="h2" className="px-3">Titular de la noticia</Row>
+            <Row as="p" className="px-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio itaque
             laboriosam
             repellat quasi voluptatum
             asperiores suscipit in iste unde. Sed temporibus aliquam quam rem illo
             asperiores
             dolore labore accusantium quo!
-        </p>
-            <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptatum,
+            </Row>
+            <Row as="p" className="mb-0 px-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptatum,
             autem
             veniam non at maxime, vero
             nobis
             accusamus officiis quia vitae explicabo! Rerum repellendus error sunt voluptatem
             harum? Libero, repellat?
-        </p>
-          </div>
-          <div className="sidebar px-3 py-4 col-md-4">
-            <h3>Categorías</h3>
-            <ul className="list-unstyled">
-              <li>Noticias de deporte</li>
-              <li>Noticias de ocio</li>
-              <li>Noticias de política</li>
-              <li>Noiticias de espectáculos</li>
-            </ul>
-            <h3>Otras cosas</h3>
-            <ul className="list-unstyled">
-              <li>Nosequé</li>
-              <li>Nosecuánto</li>
-              <li className="mb-0">Talycual</li>
-            </ul>
-          </div>
-        </main>
-        <footer className="mb-0 px-3 py-4">
-          <p className="mb-0 text-center"> Texto del footer</p>
-        </footer>
+            </Row>
+          </Col>
+          <Col md={4} className="sidebar px-3 py-4">
+            <Row as="h3" className="px-3">Categorías</Row>
+            <Col as="ul" className="list-unstyled">
+              <Row>Noticias de deporte</Row>
+              <Row>Noticias de ocio</Row>
+              <Row>Noticias de política</Row>
+              <Row>Noiticias de espectáculos</Row>
+            </Col>
+            <Row as="h3" className="px-3">Otras cosas</Row>
+            <Col as="ul" className="list-unstyled">
+              <Row>Nosequé</Row>
+              <Row>Nosecuánto</Row>
+              <Row className="mb-0">Talycual</Row>
+            </Col>
+          </Col>
+        </Row>
+        <Row as="footer" className="mb-0 px-3 py-4">
+          <Col className="mb-0 text-center"> Texto del footer</Col>
+        </Row>
       </Container>
     </Container >
   );
